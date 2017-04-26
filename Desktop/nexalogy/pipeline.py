a0b9da1177c5736597b33d6fff2036dd65970014
Mustyy <mustafa@alumni.ie.edu>
2017-04-25 22:33:44 -0400

    initial pipeline

---
+++ Desktop/nexalogy/pipeline.py
@@ -0,0 +1,256 @@
+#get up spaCy
+
+from __future__ import unicode_literals
+from spacy.en import English
+import spacy
+parser = English()
+
+from spacy.symbols import *
+from nltk.stem.wordnet import WordNetLemmatizer
+
+
+SUBJECTS = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
+OBJECTS = ["dobj", "dative", "attr", "oprd"]
+import nltk
+with open('/Users/Mustafa/desktop/sentences.txt', 'r') as sentences:
+    text = sentences.read()
+    sents = nltk.sent_tokenize(text)
+
+parsedData = parser(text)
+
+# Let's look at the tokens
+# All you have to do is iterate through the parsedData
+# Each token is an object with lots of different properties
+# A property with an underscore at the end returns the string representation
+# while a property without the underscore returns an index (int) into spaCy's vocabulary
+# The probability estimate is based on counts from a 3 billion word
+# corpus, smoothed using the Simple Good-Turing method.
+for i, token in enumerate(parsedData):
+    print("original:", token.orth, token.orth_)
+    print("lowercased:", token.lower, token.lower_)
+    print("lemma:", token.lemma, token.lemma_)
+    print("shape:", token.shape, token.shape_)
+    print("prefix:", token.prefix, token.prefix_)
+    print("suffix:", token.suffix, token.suffix_)
+    print("log probability:", token.prob)
+    print("Brown cluster id:", token.cluster)
+    print("----------------------------------------")
+    if i > 1:
+        break
+
+# Let's look at the sentences
+sents = []
+# the "sents" property returns spans
+# spans have indices into the original string
+# where each index value represents a token
+for span in parsedData.sents:
+    # go from the start to the end of each span, returning each token in the sentence
+    # combine each token using join()
+    sent = ''.join(parsedData[i].string for i in range(span.start, span.end)).strip()
+    sents.append(sent)
+
+for sentence in sents:
+    print(sentence)
+
+# Let's look at the part of speech tags of the first sentence
+for span in parsedData.sents:
+    sent = [parsedData[i] for i in range(span.start, span.end)]
+    break
+
+for token in sent:
+    print(token.orth_, token.pos_)
+
+    
+#Let's look at the dependencies of this example:
+example = text
+
+
+parsedEx = parser(example)
+# shown as: original token, dependency tag, head word, left dependents, right dependents
+for token in parsedEx:
+    print(token.orth_, token.dep_, token.head.orth_, [t.orth_ for t in token.lefts], [t.orth_ for t in token.rights])
+
+
+
+# Let's look at the named entities of this example:
+example = text
+
+parsedEx = parser(example)
+for token in parsedEx:
+    print(token.orth_, token.ent_type_ if token.ent_type_ != "" else "(not an entity)")
+
+print("-------------- entities only ---------------")
+# if you just want the entities and nothing else, you can do access the parsed examples "ents" property like this:
+ents = list(parsedEx.ents)
+for entity in ents:
+    print(entity.label, entity.label_, ' '.join(t.orth_ for t in entity))
+
+def getSubsFromConjunctions(subs):
+    moreSubs = []
+    for sub in subs:
+        # rights is a generator
+        rights = list(sub.rights)
+        rightDeps = {tok.lower_ for tok in rights}
+        if "and" in rightDeps:
+            moreSubs.extend([tok for tok in rights if tok.dep_ in SUBJECTS or tok.pos_ == "NOUN"])
+            if len(moreSubs) > 0:
+                moreSubs.extend(getSubsFromConjunctions(moreSubs))
+    return moreSubs
+
+def getObjsFromConjunctions(objs):
+    moreObjs = []
+    for obj in objs:
+        # rights is a generator
+        rights = list(obj.rights)
+        rightDeps = {tok.lower_ for tok in rights}
+        if "and" in rightDeps:
+            moreObjs.extend([tok for tok in rights if tok.dep_ in OBJECTS or tok.pos_ == "NOUN"])
+            if len(moreObjs) > 0:
+                moreObjs.extend(getObjsFromConjunctions(moreObjs))
+    return moreObjs
+
+def getVerbsFromConjunctions(verbs):
+    moreVerbs = []
+    for verb in verbs:
+        rightDeps = {tok.lower_ for tok in verb.rights}
+        if "and" in rightDeps:
+            moreVerbs.extend([tok for tok in verb.rights if tok.pos_ == "VERB"])
+            if len(moreVerbs) > 0:
+                moreVerbs.extend(getVerbsFromConjunctions(moreVerbs))
+    return moreVerbs
+
+def findSubs(tok):
+    head = tok.head
+    while head.pos_ != "VERB" and head.pos_ != "NOUN" and head.head != head:
+        head = head.head
+    if head.pos_ == "VERB":
+        subs = [tok for tok in head.lefts if tok.dep_ == "SUB"]
+        if len(subs) > 0:
+            verbNegated = isNegated(head)
+            subs.extend(getSubsFromConjunctions(subs))
+            return subs, verbNegated
+        elif head.head != head:
+            return findSubs(head)
+    elif head.pos_ == "NOUN":
+        return [head], isNegated(tok)
+    return [], False
+
+def isNegated(tok):
+    negations = {"no", "not", "n't", "never", "none"}
+    for dep in list(tok.lefts) + list(tok.rights):
+        if dep.lower_ in negations:
+              return True
+    return False
+
+def findSVs(tokens):
+    svs = []
+    verbs = [tok for tok in tokens if tok.pos_ == "VERB"]
+    for v in verbs:
+        subs, verbNegated = getAllSubs(v)
+        if len(subs) > 0:
+            for sub in subs:
+                svs.append((sub.orth_, "!" + v.orth_ if verbNegated else v.orth_))
+    return svs
+
+def getObjsFromPrepositions(deps):
+    objs = []
+    for dep in deps:
+        if dep.pos_ == "ADP" and dep.dep_ == "prep":
+            objs.extend([tok for tok in dep.rights if tok.dep_  in OBJECTS or (tok.pos_ == "PRON" and tok.lower_ == "me")])
+    return objs
+
+def getObjsFromAttrs(deps):
+    for dep in deps:
+        if dep.pos_ == "NOUN" and dep.dep_ == "attr":
+            verbs = [tok for tok in dep.rights if tok.pos_ == "VERB"]
+            if len(verbs) > 0:
+                for v in verbs:
+                    rights = list(v.rights)
+                    objs = [tok for tok in rights if tok.dep_ in OBJECTS]
+                    objs.extend(getObjsFromPrepositions(rights))
+                    if len(objs) > 0:
+                        return v, objs
+    return None, None
+
+def getObjFromXComp(deps):
+    for dep in deps:
+        if dep.pos_ == "VERB" and dep.dep_ == "xcomp":
+            v = dep
+            rights = list(v.rights)
+            objs = [tok for tok in rights if tok.dep_ in OBJECTS]
+            objs.extend(getObjsFromPrepositions(rights))
+            if len(objs) > 0:
+                return v, objs
+    return None, None
+
+def getAllSubs(v):
+    verbNegated = isNegated(v)
+    subs = [tok for tok in v.lefts if tok.dep_ in SUBJECTS and tok.pos_ != "DET"]
+    if len(subs) > 0:
+        subs.extend(getSubsFromConjunctions(subs))
+    else:
+        foundSubs, verbNegated = findSubs(v)
+        subs.extend(foundSubs)
+    return subs, verbNegated
+
+def getAllObjs(v):
+    # rights is a generator
+# rights is a generator
+    rights = list(v.rights)
+    objs = [tok for tok in rights if tok.dep_ in OBJECTS]
+    objs.extend(getObjsFromPrepositions(rights))
+
+    #potentialNewVerb, potentialNewObjs = getObjsFromAttrs(rights)
+    #if potentialNewVerb is not None and potentialNewObjs is not None and len(potentialNewObjs) > 0:
+    #    objs.extend(potentialNewObjs)
+    #    v = potentialNewVerb
+
+    potentialNewVerb, potentialNewObjs = getObjFromXComp(rights)
+    if potentialNewVerb is not None and potentialNewObjs is not None and len(potentialNewObjs) > 0:
+        objs.extend(potentialNewObjs)
+        v = potentialNewVerb
+    if len(objs) > 0:
+        objs.extend(getObjsFromConjunctions(objs))
+    return v, objs
+
+def findSVOs(tokens):
+    svos = []
+    verbs = [tok for tok in tokens if tok.pos_ == "VERB" and tok.dep_ != "aux"]
+    for v in verbs:
+        subs, verbNegated = getAllSubs(v)
+        # hopefully there are subs, if not, don't examine this verb any longer
+        if len(subs) > 0:
+            v, objs = getAllObjs(v)
+            for sub in subs:
+                for obj in objs:
+                    objNegated = isNegated(obj)
+                    svos.append((sub.lower_, "!" + v.lower_ if verbNegated or objNegated else v.lower_, obj.lower_))
+    return svos
+
+    svos = findSVOs(tokens)
+    wnl = WordNetLemmatizer()
+    passed = []
+    for s, v, o in svos:
+        s = wnl.lemmatize(s)
+        v = "!" + wnl.lemmatize(v[1:], 'v') if v[0] == "!" else wnl.lemmatize(v, 'v')
+        o = "!" + wnl.lemmatize(o[1:]) if o[0] == "!" else wnl.lemmatize(o)
+    return passed
+
+def printDeps(toks):
+    for tok in toks:
+        print(tok.orth_, tok.dep_, tok.pos_, tok.head.orth_, [t.orth_ for t in tok.lefts], [t.orth_ for t in tok.rights])
+
+def testSVOs():
+    nlp = English()
+
+    tok = nlp(text)
+    svos = findSVOs(tok)
+    printDeps(tok)
+    print(svos)
+
+def main():
+    testSVOs()
+
+if __name__ == "__main__":
+    main()
+
